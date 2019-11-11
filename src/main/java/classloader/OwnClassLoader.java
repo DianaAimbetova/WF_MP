05ef11e2e4fdce546f75f0958c85e5d06d27bf09
Diana Aimbetova <diana_aimbetova@epam.com>
2019-11-11 10:04:58 +0600

    sonar fixes

--- src/main/java/classloader/OwnClassLoader.java
+++ src/main/java/classloader/OwnClassLoader.java
@@ -2,6 +2,8 @@ package classloader;
 
 import org.apache.commons.collections.MapIterator;
 import org.apache.commons.collections.map.LRUMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -10,22 +12,16 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 
-/**
- * Custom ClassLoader by extension of the standard ClassLoader.
- * @author Diana_Aimbetova
- * @version 4.0
- */
-public class OwnClassLoader extends ClassLoader {
-    private long timeToLive;
-    private LRUMap cacheMap;
 
+public class OwnClassLoader extends ClassLoader {
+    private int timeToLive;
+    private final LRUMap cacheMap;
+    private static Logger logger = LogManager.getLogger(OwnClassLoader.class);
 
     protected class CacheObject {
-        public long lastAccessed = System.currentTimeMillis();
-        public Class value;
+        private long lastAccessed = System.currentTimeMillis();
 
         protected CacheObject(Class value) {
-            this.value = value;
         }
     }
 
@@ -33,22 +29,19 @@ public class OwnClassLoader extends ClassLoader {
 
         timeToLive = 600 * 1000;
         cacheMap = new LRUMap();
-
-            Thread t = new Thread(new Runnable() {
-                public void run() {
-                    while (true) {
-                        try {
-                            Thread.sleep(timeToLive);
-                        } catch (InterruptedException ex) {
-                        }
-                        cleanup();
-                    }
+        if (timeToLive > 0) {
+            Thread t = new Thread(() -> {
+                try {
+                    Thread.sleep(timeToLive);
+                } catch (InterruptedException ex) {
+                    logger.error(ex);
+                    Thread.currentThread().interrupt();
                 }
+                cleanup();
             });
-
             t.setDaemon(true);
             t.start();
-
+        }
     }
 
     public void put(String key, Class value) {
@@ -58,12 +51,12 @@ public class OwnClassLoader extends ClassLoader {
     }
 
     public Class addClass(String name) throws ClassNotFoundException {
-        if(!contains(name)) {
+        if (!contains(name)) {
             Class cl = findClass(name);
-            put(name,cl);
+            put(name, cl);
             return cl;
-        } else{
-            System.out.println("You've already added this class.");
+        } else {
+            logger.info("You've already added this class.");
         }
         return null;
     }
@@ -72,17 +65,15 @@ public class OwnClassLoader extends ClassLoader {
     public Class updateClass(String name) throws ClassNotFoundException {
         Class clazz = Class.forName(name);
         final URL[] urls = new URL[1];
-        urls[0] =  clazz.getProtectionDomain().getCodeSource().getLocation();
+        urls[0] = clazz.getProtectionDomain().getCodeSource().getLocation();
         final ClassLoader delegateParent = clazz.getClassLoader().getParent();
         try (final URLClassLoader cl = new URLClassLoader(urls, delegateParent)) {
-            final Class<?> reloadedClazz = cl.loadClass(clazz.getName());
-            return reloadedClazz;
+            return cl.loadClass(clazz.getName());
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e);
         }
         return null;
     }
-
 
 
     public boolean contains(String key) {
@@ -97,46 +88,25 @@ public class OwnClassLoader extends ClassLoader {
         }
     }
 
-    /**
-     * Overrided method from ClassLoader it returns class
-     * @param name
-     * @return
-     * @throws ClassNotFoundException
-     */
     @Override
     public Class<?> findClass(String name) throws ClassNotFoundException {
         byte[] bt = loadClassFromFile(name);
         return defineClass(name, bt, 0, bt.length);
     }
 
-    /**
-     * Custom method to read class and returns it as array of bytes
-     * @param className
-     * @return
-     */
+
     private byte[] loadClassFromFile(String className) {
-        InputStream is = getClass().getClassLoader().getResourceAsStream(className.replace(".", "/")+".class");
+        InputStream is = getClass().getClassLoader().getResourceAsStream(className.replace(".", "/") + ".class");
         ByteArrayOutputStream byteSt = new ByteArrayOutputStream();
-        int len =0;
+        int len = 0;
         try {
-            while((len=is.read())!=-1){
+            while ((len = is.read()) != -1) {
                 byteSt.write(len);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e);
         }
         return byteSt.toByteArray();
-    }
-
-    public void cleanup(String name) {
-        synchronized (cacheMap) {
-            CacheObject c = (CacheObject) cacheMap.get(name);
-            if (c != null) {
-                cacheMap.remove(name);
-            } else {
-                c.lastAccessed = System.currentTimeMillis();
-            }
-        }
     }
 
     public void cleanup() {
@@ -169,8 +139,6 @@ public class OwnClassLoader extends ClassLoader {
             Thread.yield();
         }
     }
-
-
 
 
 }
--- src/main/java/runner/Runner.java
+++ src/main/java/runner/Runner.java
@@ -1,24 +1,12 @@
 package runner;
 
 import classloader.OwnClassLoader;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 
-/**
- * Class to load the classes entities.Cat and entities.Dog into collection Animals by your own class loader and run the methods "Play", "Voice".
- *
- * @author Diana_Aimbetova
- * @version 4.0
- */
 public class Runner {
-    private static Logger logger = LogManager.getLogger(Runner.class);
-    private static  int swValue;
 
     public static void main(String[] args) {
         OwnClassLoader cl = new OwnClassLoader();
@@ -33,7 +21,7 @@ public class Runner {
 
     private static void run(OwnClassLoader cl, Scanner scanner) {
         System.out.println("Please choose method or enter 3 to exit...");
-        swValue = scanner.nextInt();
+        int swValue = scanner.nextInt();
         try {
             switch (swValue) {
                 case 1:
@@ -42,13 +30,19 @@ public class Runner {
                     Class myClass = cl.addClass(className);
                     Arrays.stream(myClass.getDeclaredFields()).forEach(field -> System.out.println(field.getName()));
                     run(cl, scanner);
+                    break;
                 case 2:
                     System.out.println("Please enter class path and name:");
                     String updateClassName = scanner.next();
                     Class myUpdatedClass = cl.updateClass(updateClassName);
                     Arrays.stream(myUpdatedClass.getDeclaredFields()).forEach(field -> System.out.println(field.getName()));
                     run(cl, scanner);
+                    break;
                 case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Wrong choice");
                     break;
             }
         } catch (ClassNotFoundException e) {
